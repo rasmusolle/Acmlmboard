1af13d0c9dc0f087130f9a08813630362fdef0d9
Rasmus <rasmusbamse@hotmail.com>
2017-10-17 16:45:28 +0200

    activeusers.php

--- activeusers.php
+++ activeusers.php
@@ -1,25 +1,25 @@
 <?php
-  require 'lib/common.php';
-  pageheader('Active users');
+require 'lib/common.php';
+pageheader('Active users');
   
-  if (isset($_GET['time'])) {
-	  $time=$_GET['time'];
-  } else {
-	  $time=$_GET['time'];
-  }
+if (isset($_GET['time'])) {
+	$time = $_GET['time'];
+} else {
+	$time = $_GET['time'];
+}
   
-  checknumeric($time);
-  if($time<1)
-    $time=86400;
+checknumeric($time);
+if($time < 1)
+	$time=86400;
 
-  $query='SELECT '.userfields('u').',u.posts,u.regdate,COUNT(*) num '
-        .'FROM users u '
-        .'LEFT JOIN posts p ON p.user=u.id '
-        .'WHERE p.date>'.(ctime()-$time).' '
-        .'GROUP BY u.id ORDER BY num DESC';
-  $users=$sql->query($query);
+$query='SELECT '.userfields('u').',u.posts,u.regdate,COUNT(*) num '
+	.'FROM users u '
+	.'LEFT JOIN posts p ON p.user=u.id '
+	.'WHERE p.date>'.(ctime()-$time).' '
+	.'GROUP BY u.id ORDER BY num DESC';
+$users=$sql->query($query);
 
-  print 'Active users during the last '.timeunits2($time).":
+print 'Active users during the last '.timeunits2($time).":
 ".      "<br>
 ".       timelink(3600).'|'.timelink(86400).'|'.timelink(604800).'|'.timelink(2592000)."
 ".      "<table cellspacing=\"0\" class=\"c1\">
@@ -30,26 +30,24 @@
 ".      "    <td class=\"b h\" width=50>Posts</td>
 ".      "    <td class=\"b h\" width=50>Total</td>
 ";
-  $post_total = 0;
-  $post_overall = 0;
-  $j=0;
-  $tr = 'n3';
-  for($i=1;$user=$sql->fetch($users);$i++){
-    $post_total+=$user['num'];
-    $post_overall+=$user['posts'];
-    $tr=($i % 2 ? 'n2': 'n3');
-    print
-        "<tr class=\"$tr\" align=\"center\">
-".      "    <td class=\"b\">$i.</td>
-".      "    <td class=\"b\" align=\"left\">".userlink($user)."</td>
-".      "    <td class=\"b\">".cdate($dateformat,$user[regdate])."</td>
-".      "    <td class=\"b\"><b>$user[num]</b></td>
-".      "    <td class=\"b\">$user[posts]</b></td>
-";
-  $j++;
-  }
+$post_total = 0;
+$post_overall = 0;
+$j = 0;
+$tr = 'n3';
+for($i = 1; $user = $sql->fetch($users); $i++) {
+	$post_total += $user['num'];
+	$post_overall += $user['posts'];
+	$tr = ($i % 2 ? 'n2': 'n3');
+	print "<tr class=\"$tr\" align=\"center\">
+	".      "<td class=\"b\">$i.</td>
+	".      "<td class=\"b\" align=\"left\">".userlink($user)."</td>
+	".      "<td class=\"b\">".cdate($dateformat,$user[regdate])."</td>
+	".      "<td class=\"b\"><b>$user[num]</b></td>
+	".      "<td class=\"b\">$user[posts]</b></td>";
+    $j++;
+}
   print "<tr class=\"h\"><td class=\"b h\" colspan=5>Totals</td></tr>
-".        "<tr class=\"$tr\" align=\"center\">
+".		"<tr class=\"$tr\" align=\"center\">
 ".      "    <td class=\"b\"><b>$j.</b></td>
 ".      "    <td class=\"b\" align=\"left\"></td>
 ".      "    <td class=\"b\"></td>
@@ -59,10 +57,10 @@
   print "</table>
 ";
 
-  pagefooter();
+pagefooter();
 
-  function timelink($timex){
-    global $time;
-    return ($time==$timex ? " ".timeunits2($timex)." " : " <a href=activeusers.php?time=$timex>".timeunits2($timex).'</a> ');
-  }
+function timelink($timex){
+	global $time;
+	return ($time==$timex ? " ".timeunits2($timex)." " : " <a href=activeusers.php?time=$timex>".timeunits2($timex).'</a> ');
+}
 ?>
