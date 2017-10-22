1bd86ee694db0db6e18566799a003bc580eeb52e
Rasmus <rasmusbamse@hotmail.com>
2017-10-22 20:43:01 +0200

    touch up ajax.php

--- ajax.php
+++ ajax.php
@@ -9,25 +9,25 @@ require 'lib/common.php';
 $response_data = array();
 
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
-switch($action) {
+switch ($action) {
 	case 'usersearch':
 		$name = isset($_REQUEST['name']) ? $sql->escape($_REQUEST['name']) : '';
 		$maxresults = isset($_REQUEST['maxresults']) ? (int)$_REQUEST['maxresults'] : 10;
 		
-		if(empty($name)) {
+		if (empty($name)) {
 			$response_data['result'] = 'error';
 			$response_data['message'] = 'Invalid name provided provided.';
 			break;
 		}
 		
-		if($maxresults <= 0) {
+		if ($maxresults <= 0) {
 			$response_data['result'] = 'error';
 			$response_data['message'] = 'No users found';
 			break;
 		}
 		
 		$results = $sql->fetchq("SELECT `id`, `name`, `displayname`, `sex` FROM `users` WHERE `name` LIKE '$name%' ORDER BY `name` LIMIT 0,$maxresults;");
-		if($results) {
+		if ($results) {
 			$response_data['result'] = 'success';
 			$response_data['data'] = $results;
 		} else {
