29418e8965dbee50c1afb06e795000c261dd5cfd
Rasmus <rasmusbamse@hotmail.com>
2017-10-30 11:14:51 +0100

    fix stats.php notices

--- stats.php
+++ stats.php
@@ -1,36 +1,36 @@
 <?php
-  require 'lib/common.php';
-  pageheader('Stats');
+require 'lib/common.php';
+pageheader('Stats');
 
-  $tstats=$sql->query('SHOW TABLE STATUS');
-  while($t=$sql->fetch($tstats))
-    $tbl[$t[Name]]=$t;
+$tstats=$sql->query('SHOW TABLE STATUS');
+while ($t = $sql->fetch($tstats))
+	$tbl[$t['Name']] = $t;
 
-  function sp($sz){
-    $b=number_format($sz,0,'.',' ');
-    return $b;
-  }
-  function tblinfo($n){
-    global $tbl;
-    $t=$tbl[$n];
-    return
+function sp($sz) {
+	$b = number_format($sz,0,'.',' ');
+	return $b;
+}
+function tblinfo($n){
+	global $tbl;
+	$t=$tbl[$n];
+	return
         "  <tr align=\"right\">
-".      "    <td class=\"b n1\" align=\"left\">$t[Name]</td>
-".      "    <td class=\"b n2\">$t[Rows]</td>
-".      "    <td class=\"b n2\">".sp($t[Avg_row_length])."</td>
-".      "    <td class=\"b n2\">".sp($t[Data_length])."</td>
-".      "    <td class=\"b n2\">".sp($t[Index_length])."</td>
-".      "    <td class=\"b n2\">".sp($t[Data_free])."</td>
-".      "    <td class=\"b n2\">".sp($t[Data_length]+$t[Index_length])."
+".      "    <td class=\"b n1\" align=\"left\">".$t['Name']."</td>
+".      "    <td class=\"b n2\">".$t['Rows']."</td>
+".      "    <td class=\"b n2\">".sp($t['Avg_row_length'])."</td>
+".      "    <td class=\"b n2\">".sp($t['Data_length'])."</td>
+".      "    <td class=\"b n2\">".sp($t['Index_length'])."</td>
+".      "    <td class=\"b n2\">".sp($t['Data_free'])."</td>
+".      "    <td class=\"b n2\">".sp($t['Data_length']+$t['Index_length'])."
 ";
-  }
+}
 
-  $fields=array('maxpostsday','maxpostshour','maxpostsdaydate','maxpostshourdate','maxusers','maxusersdate');
-  foreach($fields as $field)
-    $$field=$sql->resultq("SELECT intval FROM misc WHERE field='$field'");
-  $maxuserstext=$sql->resultq('SELECT txtval FROM misc WHERE field="maxuserstext"');
+$fields = array('maxpostsday','maxpostshour','maxpostsdaydate','maxpostshourdate','maxusers','maxusersdate');
+foreach($fields as $field)
+	$$field = $sql->resultq("SELECT intval FROM misc WHERE field='$field'");
+$maxuserstext = $sql->resultq('SELECT txtval FROM misc WHERE field="maxuserstext"');
 
-  print "<table cellspacing=\"0\" class=\"c1\">
+print "<table cellspacing=\"0\" class=\"c1\">
 ".      "  <tr class=\"h\">
 ".      "    <td class=\"b h\" width=180>Records</td>
 ".      "    <td class=\"b h\">&nbsp;</td>
@@ -77,41 +77,30 @@
 ".      "    <td class=\"b\">New views</td>
 ";
 
-//".       tblinfo('pmsgs_text')."
-//".       tblinfo('pmsgs')."
-//".       tblinfo('postlayouts')."
-//".       tblinfo('poll')."
-//".       tblinfo('poll_choices')."
-//".       tblinfo('pollvotes')."
-//".       tblinfo('announcements')."
-//".       tblinfo('forumread')."
-//".       tblinfo('userratings')."
-//".       tblinfo('favorites')."
+$users = 0;
+$posts = 0;
+$threads = 0;
+$views = 0;
+$stats = $sql->query('SELECT * FROM dailystats');
+while ($day = $sql->fetch($stats)) {
+	?>
+	<tr align="center">
+		<td class="b n1"><?php echo $day['date']; ?></td>
+		<td class="b n2"><?php echo $day['users']; ?></td>
+		<td class="b n2"><?php echo $day['posts']; ?></td>
+		<td class="b n2"><?php echo $day['threads']; ?></td>
+		<td class="b n2"><?php echo $day['views']; ?></td>
+		<td class="b n2"><?php echo $day['users'] - $users; ?></td>
+		<td class="b n2"><?php echo $day['posts'] - $posts; ?></td>
+		<td class="b n2"><?php echo $day['threads'] - $threads; ?></td>
+		<td class="b n2"><?php echo $day['views'] - $views; ?></td>
+	<?php
+	$users = $day['users'];
+	$posts = $day['posts'];
+	$threads = $day['threads'];
+	$views = $day['views'];
+}
+print "</table>";
 
-  $users=0;
-  $posts=0;
-  $threads=0;
-  $views=0;
-  $stats=$sql->query('SELECT * FROM dailystats');
-  while($day=$sql->fetch($stats)){
-    print
-        "  <tr align=\"center\">
-".      "    <td class=\"b n1\">$day[date]</td>
-".      "    <td class=\"b n2\">$day[users]</td>
-".      "    <td class=\"b n2\">$day[posts]</td>
-".      "    <td class=\"b n2\">$day[threads]</td>
-".      "    <td class=\"b n2\">$day[views]</td>
-".      "    <td class=\"b n2\">".($day[users]-$users)."</td>
-".      "    <td class=\"b n2\">".($day[posts]-$posts)."</td>
-".      "    <td class=\"b n2\">".($day[threads]-$threads)."</td>
-".      "    <td class=\"b n2\">".($day[views]-$views)."</td>
-";
-    $users=$day[users];
-    $posts=$day[posts];
-    $threads=$day[threads];
-    $views=$day[views];
-  }
-  print "</table>";
-
-  pagefooter();
+pagefooter();
 ?>
