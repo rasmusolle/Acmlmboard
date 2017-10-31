7ab2f7cb39d23991f345339ba74010f2c101c49c
Rasmus <rasmusbamse@hotmail.com>
2017-10-31 11:25:46 +0100

    common.css

--- lib/common.php
+++ lib/common.php
@@ -329,20 +329,10 @@ function pageheader($pagetitle = "", $fid = 0) {
       $config[meta]
       <link rel=\"icon\" type=\"image/png\" href=\"$favicon\">
       <style>
-       .spoiler1
-        {
-         border: 1px dotted rgba(255,255,255,0.5);
-        }
-       .spoiler2
-        {
-         opacity: 0;
-        }
-       .spoiler2:hover
-        {
-         opacity: 1;
-        }
+
       </style>
       <link rel=\"stylesheet\" href=\"css/$themefile\">
+      <link rel=\"stylesheet\" href=\"css/common.css\">
       <link href=\"lib/prettify/sunburst.css\" type=\"text/css\" rel=\"stylesheet\" />
       <script type=\"text/javascript\" src=\"lib/prettify/prettify.js\"></script>
       </head>
