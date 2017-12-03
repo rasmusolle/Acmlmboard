67cc3bb909cfb3c3f3b0af30f7566649eacd9e91
Rasmus <rasmusbamse@hotmail.com>
2017-12-03 19:46:21 +0100

    oh yeah, this as well

--- editattn.php
+++ editattn.php
@@ -4,7 +4,6 @@ include('lib/common.php');
 
 $action = isset($_POST['action']) ? $_POST['action'] : '';
 $attndata = '';
-$mockboardlogo = '';
 
 if (!has_perm('edit-attentions-box')) {
 	pageheader('Nothing here.');
@@ -14,47 +13,28 @@ if (!has_perm('edit-attentions-box')) {
 		$sql->query("UPDATE misc SET txtval='" . $_POST['txtval'] . "' WHERE field='attention'");
 	}
 
-	if ($action == "Preview") {
-		$attndata = stripslashes($_POST['txtval']);
+	$attndata = $sql->resultq("SELECT txtval FROM misc WHERE field='attention'");
 
-		$previewattn = "
-                 <table cellspacing=\"0\" class=\"c1\" width=\"100%\" align=\"center\">
-                   <tr class=\"h\">
-                      <td class=\"b h\"><font color='red'><i>Preview </i></font>$config[atnname] $ae</td>
-                    <tr class=\"n2\" align=\"center\">
-                      <td class=\"b sfont\">" . $attndata . "
-                      </td>
-                 </table>";
-		$mockboardlogo = "
-       <table cellspacing=\"0\" width=100%>
-         <tr align=\"center\">
-           <td class=\"b\" style=\"border:none!important\" valign=\"center\"></td>
-           <td class=\"b\" style=\"border:none!important\" valign=\"center\" width=\"300\">
-             $previewattn
-           </td>
-       </table><br/>";
-	} else
-		$attndata = $sql->resultq("SELECT txtval FROM misc WHERE field='attention'");
-
-	$pageheadtxt = "Edit " . $config['atnname'];
+	$pageheadtxt = "Edit news";
 	pageheader($pageheadtxt);
-	//echo $previewattn."<br />";
-	echo $mockboardlogo;
 
-	echo "<form action=\"editattn.php\" method=\"post\">
-" . "<table cellspacing=\"0\" class=\"c1\">
-" . "  <tr class=\"h\">
-" . "    <td class=\"b h\">
-" . "      Edit $config[atnname]
-" . "  <tr class=\"n1\">
-" . "    <td class=\"b\">
-" . "      <textarea wrap=\"virtual\" name='txtval' rows=8 cols=120>" . $attndata . "</textarea>
-" . "  <tr class=\"n1\">
-" . "    <td class=\"b n1\" align=\"center\">
-" . "      <input type=\"submit\" class=\"submit\" name=action value=Preview>
-" . "      <input type=\"submit\" class=\"submit\" name=action value=Submit>
-" . "</table> </form>";
+	?>
+	<form action="editattn.php" method="post">
+		<table class="c1">
+			<tr class="h"><td class="b h">Edit news box</td></tr>
+			<tr class="n1">
+				<td class="b">
+					<textarea wrap="virtual" name="txtval" rows="8" cols="120"><?php echo $attndata; ?></textarea>
+				</td>
+			</tr>
+			<tr class="n1">
+				<td class="b n1" align="center">
+					<input type="submit" class="submit" name="action" value="Submit">
+				</td>
+			</tr>	
+		</table>
+	</form>
+	<?php
 }
-
 pagefooter();
 ?>
