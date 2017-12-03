7857ec56e11f5b7522dd2acb5189450ff8207d08
Rasmus <rasmusbamse@hotmail.com>
2017-12-03 19:46:49 +0100

    clean up management.php

--- management.php
+++ management.php
@@ -1,12 +1,10 @@
 <?php
 require 'lib/common.php';
 
-if (!has_perm('manage-board')) error("Error", "You have no permissions to do this!<br> <a href=./>Back to main</a>");
-
 pageheader('Management');
 
 $mlinks = array();
-$mlinks[] = array('url' => "updatethemes.php", 'title' => 'Update Themes');
+//$mlinks[] = array('url' => "updatethemes.php", 'title' => 'Update Themes');
 if (has_perm("edit-forums")) 
   $mlinks[] = array('url' => "manageforums.php", 'title' => 'Manage forums');
 if (has_perm("edit-ip-bans")) 
@@ -31,16 +29,19 @@ if (has_perm("admin-tools-access"))
 //Inspired by Tierage's dashboard.php in Blargboard Plus. - SquidEmpress
 $mlinkstext = '';
 foreach ($mlinks as $l)
-	$mlinkstext .= ($mlinkstext?' ':'')."<a href=\"{$l['url']}\"</a><input type=\"submit\" class=\"submit\" name=action value='{$l['title']}'></a>";
+	$mlinkstext .= ($mlinkstext?' ':'').'&nbsp;<a href="' . $l['url'] . '"></a><input type="submit" class="submit" name=action value="' . $l['title'] . '"></a>';
 
-echo "<table cellspacing=\"0\" class=\"c1\">
-".    "  <tr class=\"h\"><td class=\"b\">Board management tools
-".    "  <tr><td class=\"b n1\" align=\"center\">
-".    "    <br>
-".    "    $mlinkstext<br>
-".    "    <br>
-".    "</table>
-";
+if ($mlinkstext == '') $mlinkstext = "There's no board management tools you're allowed to use.";
 
+?>
+<table class="c1">
+	<tr class="h"><td class="b">Board management tools</td></tr>
+	<tr>
+		<td class="b n1" align="center">
+			<br><?php echo $mlinkstext; ?><br><br>
+		</td>
+	</tr>
+</table>
+<?php
 pagefooter();
 ?>
