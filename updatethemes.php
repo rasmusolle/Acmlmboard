76f1149ae49e1014069a95a962b96b960820311e
Rasmus <rasmusbamse@hotmail.com>
2017-12-27 22:05:38 +0100

    Cleanup updatethemes.php

--- updatethemes.php
+++ updatethemes.php
@@ -3,24 +3,21 @@
 require("lib/common.php");
 pageheader();
 
-
-echo "Scanning for new themes...<br>";
+echo "Scanning...<br>";
 
 $files = scandir("css");
 sort($files);
-foreach($files as $f)
-{
-	if($f[0] == ".")
+foreach ($files as $f) {
+	if ($f[0] == ".")
 		continue;
 	$snarf = file_get_contents("css/".$f);
 	$snarf = str_replace("\r\n", "\n", $snarf);
-	if(preg_match("~/* META\n(.*?)\n(.*?)\n*/\n~s", $snarf, $matches))
-	{
+	if (preg_match("~/* META\n(.*?)\n(.*?)\n*/\n~s", $snarf, $matches)) {
 		$n = $matches[1];
 		$d = substr($matches[2], 0, -2);
-		//echo "Got a hit on ".$f."! Its name is \"$n\", description \"$d\".<br>";
+		echo "Got a hit on ".$f."! Its name is \"$n\", description \"$d\".<br>";
 		$f2 = str_replace(".css", "", str_replace(".php", "", $f));
-		if($d != "")
+		if ($d != "")
 			$newlist[] = array($n, $f2, $d);
 		else
 			$newlist[] = array($n, $f2);
