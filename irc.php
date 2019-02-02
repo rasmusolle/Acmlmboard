e8059183b9b53685da99aa13c9eed3778d8c5c92
Rasmus <rasmusbamse@hotmail.com>
2019-02-02 17:19:31 +0100

    Remove IRC command quick help.

--- irc.php
+++ irc.php
@@ -13,19 +13,4 @@ pageheader();
 		</td>
 	</tr>
 </table>
-<br>
-<table class="c1">
-	<tr class="h">
-		<td class="b h">Quick Help - Commands</td>
-	<tr>
-		<td class="b n1">		
-			<kbd>/nick [name]</kbd> - changes your name
-			<br><kbd>/me [action]</kbd> - does an action (try it)
-			<br><kbd>/msg [name] [message]</kbd> - send a private message to another user
-			<br><kbd>/join [#channel]</kbd> - joins a channel
-			<br><kbd>/part [#channel]</kbd> - leaves a channel
-			<br><kbd>/quit [message]</kbd> - disconnects from the server
-		</td>
-	</tr>
-</table>
 <?php pagefooter(); ?>
