267d6895487c8d7b49f915d8ef36a0aba4fad1f8
Rasmus <rasmusbamse@hotmail.com>
2019-03-04 14:18:32 +0100

    Redesign online users page.

--- online.php
+++ online.php
@@ -6,98 +6,32 @@ pageheader('Online users');
 $time = (isset($_GET['time']) ? $_GET['time'] : null);
 checknumeric($time);
 
-if (!$time)
-	$time = 300;
+if (!$time) $time = 300;
 
 $users = $sql->query("SELECT * FROM users WHERE lastview > ".(time()-$time)." ORDER BY lastview DESC");
-$guests = $sql->query("SELECT g.* FROM guests g WHERE g.date > ".(time()-$time)." AND g.bot=0 ORDER BY g.date DESC");
-$bots = $sql->query("SELECT * FROM guests WHERE date > ".(time()-$time)." AND bot=1 ORDER BY date DESC");
 ?>
-Online users during the last <?=str_replace('.', '', timeunits2($time)) ?>:
-<div style="margin-top: 3px; margin-bottom: 3px; display:block">
-	<?=timelink(60).'|'.timelink(300).'|'.timelink(900).'|'.timelink(3600).'|'.timelink(86400) ?>
-</div>
+<table class="c1" style="width:auto">
+	<tr class="h"><td class="b">Online users during the last <?=str_replace('.', '', timeunits2($time)) ?>:</td></tr>
+	<tr class="n1"><td class="b n1"><?=timelink(60).'|'.timelink(300).'|'.timelink(3600).'|'.timelink(86400) ?></td></tr>
+</table><br>
 <table class="c1">
 	<tr class="h">
 		<td class="b h" width="30">#</td>
-		<td class="b h">Name</td>
-		<td class="b h" width="90">Last view</td>
-		<td class="b h" width="140">Last post</td>
-		<?=(has_perm('view-user-urls') ? '<td class="b h">URL</td>' : '') ?>
+		<td class="b h" width="230">Name</td>
+		<td class="b h" width="80">Last view</td>
+		<td class="b h">URL</td>
 		<?=(has_perm('view-post-ips') ? '<td class="b h" width="120">IP</td>' : '') ?>
-		<td class="b h" width="50">Posts</td>
 	</tr>
 <?php
 for ($i = 1; $user = $sql->fetch($users); $i++) {
-	$tr = ($i % 2 ? 'n2' : 'n3');
+	$tr = ($i % 2 ? 'n1' : 'n2');
 	?>
 	<tr class="<?=$tr ?> center">
-		<td class="b n1"><?=$i ?>.</td>
+		<td class="b"><?=$i ?>.</td>
 		<td class="b left"><?=userlink($user) ?></td>
 		<td class="b"><?=cdate($loguser['timeformat'], $user['lastview']) ?></td>
-		<td class="b"><?=($user['lastpost'] ? cdate($dateformat, $user['lastpost']) : '-') ?></td>
-		<?=(has_perm('view-user-urls') ? '<td class="b left">'
-			. ($user['url'] ? "<a href=$user[url]>" . str_replace(array('%20','_'), ' ', $user['url']) . '</a>' : '-')
-			. "</td>" : '') ?>
+		<td class="b left"><?=($user['url'] ? "<a href=$user[url]>" . str_replace(array('%20','_'), ' ', $user['url']) . '</a>' : '-') ?></td>
 		<?=(has_perm("view-post-ips") ? '<td class="b">'.$user['ip'].'</td>':'') ?>
-		<td class="b"><?=$user['posts'] ?></td>
-	</tr>
-<?php } ?>
-</table><br>
-Guests:
-<table class="c1">
-	<tr class="h">
-		<td class="b h" width="30">#</td>
-		<td class="b h" width="70" style="min-width: 150px;">User agent (Browser)</td>
-		<td class="b h" width="70">Last view</td>
-		<td class="b h">URL</td>
-		<?=(has_perm("view-post-ips") ? '<td class="b h" width="120">IP</td>' : '') ?>
-	</tr>
-<?php
-for ($i = 1; $guest = $sql->fetch($guests); $i++) {
-	$tr = ($i % 2 ? 'n2' : 'n3');
-	?>
-	<tr class="<?=$tr ?> center">
-		<td class="b n1"><?=$i ?>.</td>
-		<td class="b left"><span title="<?=htmlspecialchars($guest['useragent']) ?>" style=white-space:nowrap><?=htmlspecialchars(substr($guest['useragent'], 0, 65)) ?></span></td>
-		<td class="b"><?=cdate($loguser['timeformat'], $guest['date']) ?></td>
-		<td class="b left">
-			<a href="<?=$guest['url'] ?>"><?=str_replace(array("%20","_"), " ", $guest['url']) ?></a>
-			<?=($guest['ipbanned'] ? " (IP banned)" : "") ?>
-		</td>
-		<?=(has_perm("view-post-ips") ? '<td class="b">' . $guest['ip'] . '</td>' : '') ?>
-	</tr>
-	<?php
-}
-?>
-</table><br>
-Bots:
-<table class="c1">
-	<tr class="h">
-		<td class="b h" width=30>#</td>
-		<td class="b h" width=70>Bot</td>
-		<td class="b h" width=70>Last view</td>
-		<td class="b h">URL</td>
-		<?=(has_perm("view-post-ips") ? '<td class="b h" width=120>IP</td>' : '') ?>
-	</tr>
-<?php
-for ($i = 1; $guest = $sql->fetch($bots); $i++) {
-	$tr = ($i % 2 ? 'n2' : 'n3');
-	?>
-	<tr class="<?=$tr ?> center">
-		<td class="b n1"><?=$i ?>.</td>
-		<td class="b left">
-			<span title="<?=htmlspecialchars($guest['useragent']) ?>" style=white-space:nowrap>
-				<?=htmlspecialchars(substr($guest['useragent'], 0, 50)) ?>
-			</span>
-		</td>
-		<td class="b"><?=cdate($loguser['timeformat'], $guest['date']) ?></td>
-		<td class="b left">
-			<span style='float:right'><?=sslicon($guest['ssl']) ?></span>
-			<a href=<?=$guest['url'] ?>><?=$guest['url'] ?></a>
-			<?=($guest['ipbanned'] ? " (IP banned)" : "") ?>
-		</td>
-		<?=(has_perm("view-post-ips") ? '<td class="b">' . $guest['ip'] . '</td>' : '') ?>
 	</tr>
 <?php } ?>
 </table><?php
@@ -108,4 +42,5 @@ function timelink($timex) {
 	global $time;
 	return ($time == $timex ? " " . timeunits2($timex) . " " : " <a href=online.php?time=$timex>" . timeunits2($timex) . '</a> ');
 }
+
 ?>
