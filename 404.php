a6680654ddaaad381972bbe11ceedd6b47e92cb7
Rasmus <rasmusbamse@hotmail.com>
2019-03-07 16:10:09 +0100

    Add 404 page.

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+require('lib/common.php');
+pageheader('404');
+?>
+<table class="c1">
+	<tr class="h"><td class="b h">404</td></tr>
+	<tr><td class="b n1">
+		<p class="center">The requested page wasn't found.</p>
+	</td></tr>
+</table>
+<?php pagefooter() ?>
